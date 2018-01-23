486891be337e89f235b209186ba8d2b8322320a7
Timbre Freeman <19438918+tztz8@users.noreply.github.com>
2018-01-23 07:40:35 -0800

    Starting the project with some sample files

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var barWidth = 20;
+var lastBar = -1;
+
+function setup() {
+  createCanvas(720, 400);
+  //colorMode(HSB, height, height, height);
+  noStroke();
+  background(0);
+}
+
+function draw() {
+  var whichBar = mouseX / barWidth;
+  if (whichBar !== lastBar) {
+    var barX = whichBar * barWidth;
+    fill(mouseY, height, height);
+    rect(barX, 0, barWidth, height);
+    lastBar = whichBar;
+  }
+}

a1599aeb29e7c72e086a9acf8029ebd74b423a8f
Timbre Freeman <180427@cvsd356.org>
2018-01-23 21:07:46 -0800

    Floor class and array chage compleat

--- sketch.js
+++ sketch.js
@@ -1,37 +1,35 @@
-var floorLight, floor;
-var seling, selingLeft, selingRight;
-var atI;
-var f1r = false;
-var f2r = false;
+var floor = new Array;
+var seling = new Array;
 
 function setup() {
   var myCanvas = createCanvas(tilesSet.width*tilesSet.size, tilesSet.height*tilesSet.size); // I save this to a variable so i can chage this about it
   myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
   background("#00F1D3");// set the background to blue
-  floorLight = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png");
-  floor = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (2).png");
-  seling = loadImage("assets/images/gameart2d_com/png/Tiles/Tile (10).png");
-  selingLeft = loadImage("assets/images/gameart2d_com/png/Tiles/Tile (9).png");
-  selingRight = loadImage("assets/images/gameart2d_com/png/Tiles/Tile (11).png");
+
+  // seting up the floor
+  var floorNormal = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (2).png");
+  var floorLight = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png");
+  for(var i = 0; i < tilesSet.width; i++){
+    if (0 == i % 2){
+      floor[i] = new tile(i*tilesSet.size,
+         height-tilesSet.size,
+         floorLight,
+         tilesSet.size);
+    }else if (Math.abs(i % 2) == 1) {
+      floor[i] = new tile(i*tilesSet.size,
+         height-tilesSet.size,
+         floorNormal,
+         tilesSet.size);
+    }else {
+      alert("something went wrong");
+      debugger;
+    }
+  }
+
 }
 
 function draw() {
-  image(selingLeft, 0, 0, tilesSet.size, tilesSet.size);
-  for (var i = 0; i < tilesSet.width;i = i + 2){
-    image(floorLight, i*tilesSet.size, height-tilesSet.size, tilesSet.size, tilesSet.size);
-    if (i != 0){
-      if (i != tilesSet.width-1){
-        image(seling, i*tilesSet.size, 0, tilesSet.size, tilesSet.size);
-      }
-    }
+  for(var i = 0; i < tilesSet.width; i++){
+    floor[i].show();
   }
-  for (var i = 1; i < tilesSet.width;i = i + 2){
-    image(floor, i*tilesSet.size, height-tilesSet.size, tilesSet.size, tilesSet.size);
-    if (i != 0){
-      if (i != tilesSet.width-1){
-        image(seling, i*tilesSet.size, 0, tilesSet.size, tilesSet.size);
-      }
-    }
-  }
-  image(selingRight, width-tilesSet.size, 0, tilesSet.size, tilesSet.size);
 }
--- tiles.js
+++ tiles.js
@@ -3,3 +3,16 @@ var tilesSet = {
   height: 5,
   size: 80
 };
+
+class tile {
+  constructor(x, y, imgLd, size){
+    this.x = x;
+    this.y = y;
+    this.img = imgLd;
+    this.size = size;
+  }
+
+  show() {
+    image(this.img, this.x, this.y, this.size, this.size);
+  }
+}

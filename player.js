c42bb54d80c023c3cc8dbd69caad257164c7ee9b
Timbre Freeman <180427@cvsd356.org>
2018-01-24 12:13:34 -0800

    Make it more efeshent

--- player.js
+++ player.js
@@ -2,14 +2,18 @@ class player {
   constructor(){
     this.x = 0;
     this.y = 0;
-    this.frame = 1;
+    this.frameAt = 1;
+    this.frame = new Array;
+    for(var i = 1; i < 11; i++){
+      this.frame[i] = loadImage("assets/images/gameart2d_com/png/Idle (" + i + ").png");
+    }
   }
 
   show(){
-    image(loadImage("assets/images/gameart2d_com/png/Idle (" + this.frame + ").png"), this.x, this.y);
-    this.frame = this.frame + 1;
-    if(this.frame > 10){
-      this.frame = 1;
+    image(this.frame[this.frameAt], this.x, this.y);
+    this.frameAt = this.frameAt + 1;
+    if(this.frameAt > 10){
+      this.frameAt = 1;
     }
   }
 }

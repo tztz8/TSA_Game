8f495a19fe92b83cfb6b6c3d215e44f153dd43ea
Timbre Freeman <180427@cvsd356.org>
2018-01-23 20:08:14 -0800

    made it esar to chane the size

--- sketch.js
+++ sketch.js
@@ -4,8 +4,12 @@ var atI;
 var f1r = false;
 var f2r = false;
 
+var widthTiles = 9;
+var heightTiles = 5;
+var sizeTiles = 80;
+
 function setup() {
-  var myCanvas = createCanvas(720, 400); // I save this to a variable so i can chage this about it
+  var myCanvas = createCanvas(widthTiles*sizeTiles, heightTiles*sizeTiles); // I save this to a variable so i can chage this about it
   myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
   background("#00F1D3");// set the background to blue
   floorLight = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png");
@@ -16,20 +20,22 @@ function setup() {
 }
 
 function draw() {
-  image(selingLeft, 0, 0, 80, 80);
-  for (var i = 0; i < 9;i = i + 2){
-    image(floorLight, i*80, height-80, 80, 80);
+  image(selingLeft, 0, 0, sizeTiles, sizeTiles);
+  for (var i = 0; i < widthTiles;i = i + 2){
+    image(floorLight, i*sizeTiles, height-sizeTiles, sizeTiles, sizeTiles);
     if (i != 0){
-      if (i != 8){
-        image(seling, i*80, 0, 80, 80);
+      if (i != widthTiles-1){
+        image(seling, i*sizeTiles, 0, sizeTiles, sizeTiles);
       }
     }
-    //debugger;
   }
-  for (var i = 1; i < 8;i = i + 2){
-    image(floor, i*80, height-80, 80, 80);
-    image(seling, i*80, 0, 80, 80);
-    //debugger;
+  for (var i = 1; i < widthTiles;i = i + 2){
+    image(floor, i*sizeTiles, height-sizeTiles, sizeTiles, sizeTiles);
+    if (i != 0){
+      if (i != widthTiles-1){
+        image(seling, i*sizeTiles, 0, sizeTiles, sizeTiles);
+      }
+    }
   }
-  image(selingRight, width-80, 0, 80, 80);
+  image(selingRight, width-sizeTiles, 0, sizeTiles, sizeTiles);
 }

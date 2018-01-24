043f9b65c4f4d9f4fc5da1b06f9cbbdb724a7c3d
Timbre Freeman <19438918+tztz8@users.noreply.github.com>
2018-01-24 08:16:10 -0800

    Not done

--- player.js
+++ player.js
@@ -2,5 +2,14 @@ class player {
   constructor(){
     this.x = 0;
     this.y = 0;
+    this.frame = 0;
+  }
+  
+  jump() {
+  }
+  
+  show(){
+    image(loadImage("assets/images/gameart2d_com/png/Idle (" + this.frame + ").png"),
+          ...
   }
 }

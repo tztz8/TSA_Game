e31669033cd06d8529997f51b387878ed03fa877
Timbre Freeman <180427@cvsd356.org>
2018-01-23 20:35:47 -0800

    add tiles.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,8 @@ var atI;
 var f1r = false;
 var f2r = false;
 
-var widthTiles = 9;
-var heightTiles = 5;
-var sizeTiles = 80;
-
 function setup() {
-  var myCanvas = createCanvas(widthTiles*sizeTiles, heightTiles*sizeTiles); // I save this to a variable so i can chage this about it
+  var myCanvas = createCanvas(tilesSet.width*tilesSet.size, tilesSet.height*tilesSet.size); // I save this to a variable so i can chage this about it
   myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
   background("#00F1D3");// set the background to blue
   floorLight = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png");
@@ -20,22 +16,22 @@ function setup() {
 }
 
 function draw() {
-  image(selingLeft, 0, 0, sizeTiles, sizeTiles);
-  for (var i = 0; i < widthTiles;i = i + 2){
-    image(floorLight, i*sizeTiles, height-sizeTiles, sizeTiles, sizeTiles);
+  image(selingLeft, 0, 0, tilesSet.size, tilesSet.size);
+  for (var i = 0; i < tilesSet.width;i = i + 2){
+    image(floorLight, i*tilesSet.size, height-tilesSet.size, tilesSet.size, tilesSet.size);
     if (i != 0){
-      if (i != widthTiles-1){
-        image(seling, i*sizeTiles, 0, sizeTiles, sizeTiles);
+      if (i != tilesSet.width-1){
+        image(seling, i*tilesSet.size, 0, tilesSet.size, tilesSet.size);
       }
     }
   }
-  for (var i = 1; i < widthTiles;i = i + 2){
-    image(floor, i*sizeTiles, height-sizeTiles, sizeTiles, sizeTiles);
+  for (var i = 1; i < tilesSet.width;i = i + 2){
+    image(floor, i*tilesSet.size, height-tilesSet.size, tilesSet.size, tilesSet.size);
     if (i != 0){
-      if (i != widthTiles-1){
-        image(seling, i*sizeTiles, 0, sizeTiles, sizeTiles);
+      if (i != tilesSet.width-1){
+        image(seling, i*tilesSet.size, 0, tilesSet.size, tilesSet.size);
       }
     }
   }
-  image(selingRight, width-sizeTiles, 0, sizeTiles, sizeTiles);
+  image(selingRight, width-tilesSet.size, 0, tilesSet.size, tilesSet.size);
 }
---
+++ tiles.js
@@ -0,0 +1,5 @@
+var tilesSet = {
+  width: 9,
+  height: 5,
+  size: 80
+};

40374d9eefa7255775bd540e9886b60377d8b564
Timbre Freeman <Timbref@gamil.com>
2018-01-23 16:47:01 -0800

    start testing tiles
    But faill on first try

--- sketch.js
+++ sketch.js
@@ -1,20 +1,12 @@
-var barWidth = 20;
-var lastBar = -1;
+var testPNG;
 
 function setup() {
   var myCanvas = createCanvas(720, 400); // I save this to a variable so i can chage this about it
   myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
-  //colorMode(HSB, height, height, height);
-  noStroke();
-  background(0);
+  background("#00F1D3");// set the background to blue
+  testPNG = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png")
 }
 
 function draw() {
-  var whichBar = mouseX / barWidth;
-  if (whichBar !== lastBar) {
-    var barX = whichBar * barWidth;
-    fill(mouseY, height, height);
-    rect(barX, 0, barWidth, height);
-    lastBar = whichBar;
-  }
+  image(testPNG, 0, 0);
 }

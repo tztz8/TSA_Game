e72f724a266041e6cd941df0f931ea6c7476a8ed
Timbre Freeman <Timbref@gamil.com>
2018-01-23 16:21:40 -0800

    Clean up some files and made it look beter

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ var barWidth = 20;
 var lastBar = -1;
 
 function setup() {
-  createCanvas(720, 400);
+  var myCanvas = createCanvas(720, 400); // I save this to a variable so i can chage this about it
+  myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
   //colorMode(HSB, height, height, height);
   noStroke();
   background(0);

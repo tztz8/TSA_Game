59c7e1a565f29b50edee40b38fe8e061b582458d
Timbre Freeman <180427@cvsd356.org>
2018-01-24 15:13:47 -0800

    fix player know can see

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
+var floorNormalimg;
+var floorLightimg;
+var selingNormalimg;
+
 var player1 = new player();
 
+function preload(){
+  player1.load();
+  floorNormalimg = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (2).png");
+  floorLightimg = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png");
+  selingNormalimg = loadImage("assets/images/gameart2d_com/png/Tiles/Tile (10).png");
+}
 function setup() {
   var myCanvas = createCanvas(tilesSet.width*tilesSet.size, tilesSet.height*tilesSet.size); // I save this to a variable so i can chage this about it
   myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
   background("#00F1D3");// set the background to blue
 
   // seting up the floor amd seling
-  setupFloor();
-  setupSeling();
+  setupFloor(floorNormalimg, floorLightimg);
+  setupSeling(selingNormalimg);
 
   // set player inshal place
   player1.x = 0;

b340a9a92cee8c919536cff5c5fc96fc43252dad
Timbre Freeman <180427@cvsd356.org>
2018-01-24 12:09:41 -0800

    fix the imges it gets

--- player.js
+++ player.js
@@ -2,14 +2,14 @@ class player {
   constructor(){
     this.x = 0;
     this.y = 0;
-    this.frame = 0;
+    this.frame = 1;
   }
 
   show(){
     image(loadImage("assets/images/gameart2d_com/png/Idle (" + this.frame + ").png"), this.x, this.y);
     this.frame = this.frame + 1;
     if(this.frame > 10){
-      this.frame = 0;
+      this.frame = 1;
     }
   }
 }

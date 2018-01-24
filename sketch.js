34ff7e81bce719a0634a2a8182d898407d6b6206
Timbre Freeman <180427@cvsd356.org>
2018-01-23 21:17:54 -0800

    move the floor and seling to tiles.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,19 @@
-var floor = new Array;
-var seling = new Array;
+
 
 function setup() {
   var myCanvas = createCanvas(tilesSet.width*tilesSet.size, tilesSet.height*tilesSet.size); // I save this to a variable so i can chage this about it
   myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
   background("#00F1D3");// set the background to blue
 
-  // seting up the floor
-  var floorNormal = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (2).png");
-  var floorLight = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png");
-  for(var i = 0; i < tilesSet.width; i++){
-    if (0 == i % 2){
-      floor[i] = new tile(i*tilesSet.size,
-         height-tilesSet.size,
-         floorLight,
-         tilesSet.size);
-    }else if (Math.abs(i % 2) == 1) {
-      floor[i] = new tile(i*tilesSet.size,
-         height-tilesSet.size,
-         floorNormal,
-         tilesSet.size);
-    }else {
-      alert("something went wrong");
-      debugger;
-    }
-  }
+  // seting up the floor amd seling
+  setupFloor();
+  setupSeling();
 
 }
 
 function draw() {
   for(var i = 0; i < tilesSet.width; i++){
     floor[i].show();
+    seling[i].show();
   }
 }
--- tiles.js
+++ tiles.js
@@ -16,3 +16,39 @@ class tile {
     image(this.img, this.x, this.y, this.size, this.size);
   }
 }
+
+
+// seting up the floor
+var floor = new Array;
+function setupFloor() {
+  var floorNormal = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (2).png");
+  var floorLight = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (1).png");
+  for(var i = 0; i < tilesSet.width; i++){
+    if (0 == i % 2){
+      floor[i] = new tile(i*tilesSet.size,
+         height-tilesSet.size,
+         floorLight,
+         tilesSet.size);
+    }else if (Math.abs(i % 2) == 1) {
+      floor[i] = new tile(i*tilesSet.size,
+         height-tilesSet.size,
+         floorNormal,
+         tilesSet.size);
+    }else {
+      alert("something went wrong");
+      debugger;
+    }
+  }
+}
+
+// seting up the floor
+var seling = new Array;
+function setupSeling() {
+  var selingNormal = loadImage("assets/images/gameart2d_com/png/Tiles/Tile (10).png");
+  for(var i = 0; i < tilesSet.width; i++){
+    seling[i] = new tile(i*tilesSet.size,
+       0,
+       selingNormal,
+       tilesSet.size);
+  }
+}

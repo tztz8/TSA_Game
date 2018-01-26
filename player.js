085e408806d09002b9f263cad3d488f5ff8c4528
Timbre Freeman <180427@cvsd356.org>
2018-01-25 17:33:39 -0800

    run but disaper still working on it

--- player.js
+++ player.js
@@ -1,22 +1,73 @@
+/*
+* States
+* 0 = Idle
+* 1 = Run
+* 2 = Jump
+*/
 class player {
   constructor(){
     this.x = 0;
     this.y = 0;
     this.frameAt = 1;
-    this.frame = new Array;
+    this.frame = new Array(3);
+    this.state = 1;
+    this.speed = 0;
+    this.size = 80;
   }
 
   load(){
-    for(var i = 1;i < 11; i++){
-      this.frame[this.frameAt] = loadImage("assets/images/gameart2d_com/png/Idle (" + i + ").png");
+    for(var i = 0; i < 3; i++){
+      this.frame[i] = new Array(10);
     }
+    // Load imges
+    for(var i = 1;i < 11; i++){ // Load Idle
+      this.frame[0][i] = loadImage("assets/images/gameart2d_com/png/Idle (" + i + ").png");
+    }
+    for(var i = 1;i < 9; i++){ // Load Run
+      this.frame[1][i] = loadImage("assets/images/gameart2d_com/png/Run (" + i + ").png");
+    }/*
+    for(var i = 1;i < 11; i++){ // Load Jump
+      this.frame[2][i] = loadImage("assets/images/gameart2d_com/png/Jump (" + i + ").png");
+    }*/
   }
 
-  show(){
-    image(this.frame[this.frameAt], this.x, this.y, 80, 80);
+  run(speed){
+    this.speed = speed;
+    this.state = 1;
+  }
+
+  stop(){
+    this.speed = 0;
+    this.state = 0;
+  }
+
+  show(widthIn){
+    // Image State
+    switch (this.state) {
+      case 0:
+        image(this.frame[0][this.frameAt], this.x, this.y);
+        break;
+      case 1:
+        image(this.frame[1][this.frameAt], this.x, this.y);
+        break;/*
+      case 2:
+        image(this.frame[2][this.frameAt], this.x, this.y, 80, 80);
+        break;*/
+      default:
+        alert("something Whent Wrong");
+        console.log(this.state);
+    }
+    // change frame
     this.frameAt = this.frameAt + 1;
     if(this.frameAt > 10){
-      this.frameAt = 1;
+      this.frameAt = 0;
+    }
+    // update pos
+    this.x = this.x + this.speed;
+    if(this.x > widthIn){
+      this.x = widthIn - this.size;
+    }else if(this.x < 0){
+      this.x = 0;
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -13,7 +13,6 @@ function preload(){
 function setup() {
   var myCanvas = createCanvas(tilesSet.width*tilesSet.size, tilesSet.height*tilesSet.size); // I save this to a variable so i can chage this about it
   myCanvas.parent('myContainer'); // I am tell the p5 scrip to put the canvas where i have the id of myContainer
-  background("#00F1D3");// set the background to blue
 
   // seting up the floor amd seling
   setupFloor(floorNormalimg, floorLightimg);
@@ -21,13 +20,14 @@ function setup() {
 
   // set player inshal place
   player1.x = 0;
-  player1.y = height / 2;
+  player1.y = 245;
 }
 
 function draw() {
+  background("#00F1D3");// set the background to blue
   for(var i = 0; i < tilesSet.width; i++){
     floor[i].show();
     seling[i].show();
   }
-  player1.show();
+  player1.show(width);
 }

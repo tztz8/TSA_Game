8c5f6868e2c50fe0fb28394353eea11adebec6d3
Timbre Freeman <180427@cvsd356.org>
2018-02-01 20:38:12 -0800

    made some chames

--- player.js
+++ player.js
@@ -7,13 +7,16 @@
 class player {
   constructor(){
     this.x = 0;
+    this.speed = 0;
+    this.yFloor = 0;
     this.y = 0;
+    this.yv = 0;
+    this.ya = -9.81;
     this.frameAt = 1;
     this.frameNumber = [];
     this.frameNumberAt = 10;
     this.frame = [];
-    this.state = 1;
-    this.speed = 0;
+    this.state = 0;
     this.size = 80;
   }
 
@@ -25,21 +28,25 @@ class player {
     this.frameNumber[0] = 10;
     for(var i = 1;i < 11; i++){ // Load Idle
       this.frame[0][i] = loadImage("assets/images/gameart2d_com/png/Idle (" + i + ").png");
-      console.log(i);
     }
     this.frameNumber[1] = 8
     for(var i = 1;i < 9; i++){ // Load Run
       this.frame[1][i] = loadImage("assets/images/gameart2d_com/png/Run (" + i + ").png");
-      console.log(i);
-    }/*
+    }
+    this.frameNumber[2] = 10
     for(var i = 1;i < 11; i++){ // Load Jump
       this.frame[2][i] = loadImage("assets/images/gameart2d_com/png/Jump (" + i + ").png");
-    }*/
+    }
   }
 
   run(speed){
     this.speed = speed;
     this.state = 1;
+  }
+
+  jump(velocity){
+    this.yv = velocity;
+    this.state = 2;
   }
 
   stop(){
@@ -48,34 +55,22 @@ class player {
   }
 
   show(widthIn){
-    // Image State
-    switch (this.state) {
-      case 0:
-        this.frameNumberAt = this.frameNumber[0];
-        image(this.frame[0][this.frameAt], this.x, this.y, this.size, this.size);
-        break;
-      case 1:
-        this.frameNumberAt = this.frameNumber[1];
-        image(this.frame[1][this.frameAt], this.x, this.y, this.size, this.size);
-        break;/*
-      case 2:
-        image(this.frame[2][this.frameAt], this.x, this.y, 80, 80);
-        break;*/
-      default:
-        alert("something Whent Wrong");
-        console.log(this.state);
-    }
+    // print Image
+    this.frameNumberAt = this.frameNumber[this.state];
+    image(this.frame[this.state][this.frameAt], this.x, this.y, this.size, this.size);
     // change frame
     this.frameAt = this.frameAt + 1;
     if(this.frameAt > this.frameNumberAt){
       this.frameAt = 1;
     }
-    // update pos
+    // update pos in the x
     this.x = this.x + this.speed;
     if(this.x > widthIn){
       this.x = widthIn - this.size;
     }else if(this.x < 0){
       this.x = 0;
     }
+    // update pos in the y
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ var selingNormalimg;
 
 var player1 = new player();
 
+var worldSpeed = 24;
+
 function preload(){
   player1.load();
   floorNormalimg = loadImage("assets/images/gameart2d_com/png/Tiles/BGTile (2).png");
@@ -21,13 +23,19 @@ function setup() {
   // set player inshal place
   player1.x = 0;
   player1.y = 245;
+  player1.yFloor = 245;
 }
 
 function draw() {
+  frameRate(worldSpeed);
   background("#00F1D3");// set the background to blue
   for(var i = 0; i < tilesSet.width; i++){
     floor[i].show();
     seling[i].show();
   }
-  player1.show(width);
+  if(player1.state == 2){
+    player1.show(width);
+  }else {
+    player1.show(width);
+  }
 }

43db1f0c1a5d7bdb0652d053c4e7b59cce3ac425
Timbre Freeman <180427@cvsd356.org>
2018-02-20 20:53:27 -0800

    fix proble and set game server to not https

--- Network.js
+++ Network.js
@@ -1,12 +1,18 @@
 var socket;
 var mPlayers = [];
+var mpCRF = false;
 
 function connect(){
-  socket = io.connect(document.getElementById("network").elements[0].value);
-  socket.on("payerM", updateMP);
-  //socket.on("newPlayer", newMPlayer);
-  setTimeout(CheckConnect, 500);
-  console.log(document.getElementById("network").elements[0].value);
+  if(!mpCRF){
+    socket = io.connect(document.getElementById("network").elements[0].value);
+    socket.on("payerM", updateMP);
+    //socket.on("newPlayer", newMPlayer);
+    setTimeout(CheckConnect, 500);
+    console.log(document.getElementById("network").elements[0].value);
+    mpCRF = true;
+  }else{
+    alert("ether try to connect or is connect refresh the page to chage url");
+  }
 }
 
 function CheckConnect () {
@@ -20,7 +26,7 @@ function CheckConnect () {
 }
 
 function updateMP(data){
-  console.log(data);
+  //console.log(data);
 
   // check if new person
   var newPlayerIF = true;

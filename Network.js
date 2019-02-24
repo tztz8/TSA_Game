49cd216f853230f007cd78ac0e93308496389249
Timbre Freeman <19438918+tztz8@users.noreply.github.com>
2019-02-24 14:44:30 -0800

    update servers with vps's

--- Network.js
+++ Network.js
@@ -67,7 +67,7 @@ function changeServerHTML(serverin){
   var serverURL;
   switch (serverin) {
     case 1:
-      serverURL = "https://home.tftinker.club:8181";
+      serverURL = "https://dcnhost.tftinker.tech:8181";
       break;
     case 2:
       serverURL = "http://localhost:8181";
@@ -76,10 +76,10 @@ function changeServerHTML(serverin){
       serverURL = "http://10.0.0.50:8181";
       break;
     case 4:
-      serverURL = "https://home.tftinker.club:8282";
+      serverURL = "https://ssdvps.tftinker.tech:8181";
       break;
     default:
-    serverURL = "https://home.tftinker.club:8181";
+    serverURL = "https://dcnhost.tftinker.tech:8181";
   }
   document.getElementById("network").elements[0].value = serverURL;
 }
